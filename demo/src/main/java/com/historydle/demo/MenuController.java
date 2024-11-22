32f86a176b735393b6c44bfb90eef4926ec07239
pierre200326 <159255704+pierre200326@users.noreply.github.com>
2024-11-22 15:51:51 +0000

    variable session pour connexion

--- demo/src/main/java/com/historydle/demo/MenuController.java
+++ demo/src/main/java/com/historydle/demo/MenuController.java
@@ -2,12 +2,20 @@ package com.historydle.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import jakarta.servlet.http.HttpSession;
+
 
 @Controller
 public class MenuController {
 
     @GetMapping("/")
-    public String accueil() {
+    public String accueil(HttpSession session) {
+        String username = (String) session.getAttribute("username");
+        if (username == null) {
+            System.out.println("Aucun utilisateur n'est connecté");
+        }else{
+            System.out.println("Utilisateur connecté : " + username);
+        }
         return "index";
     }
 
