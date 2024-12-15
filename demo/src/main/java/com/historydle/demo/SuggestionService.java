226b2a278e204ac0d2d22f851cb0e7691085ad2b
pierre200326 <nichelepierre@gmail.com>
2024-12-15 12:49:50 +0100

    Ajout du voir Suggestion

--- demo/src/main/java/com/historydle/demo/SuggestionService.java
+++ demo/src/main/java/com/historydle/demo/SuggestionService.java
@@ -1,5 +1,7 @@
 package com.historydle.demo;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.historydle.demo.Entity.PersonnageSuggestion;
@@ -8,7 +10,7 @@ import com.historydle.demo.Repository.PersonnageSuggestionRepository;
 @Service
 public class SuggestionService {
     
-    private PersonnageSuggestionRepository personnageSuggestionRepository;
+    private final PersonnageSuggestionRepository personnageSuggestionRepository;
 
      public SuggestionService(PersonnageSuggestionRepository personnageSuggestionRepository) {
         this.personnageSuggestionRepository = personnageSuggestionRepository;
@@ -17,6 +19,10 @@ public class SuggestionService {
      public PersonnageSuggestion inscrirePersonnage(String nom,String genre,String pays,String continent, String domaine, int siecle, String citation,String pseudo) {
         PersonnageSuggestion personnageSuggestion = new PersonnageSuggestion(nom,genre,pays,continent,domaine,siecle,citation,pseudo);
         return personnageSuggestionRepository.save(personnageSuggestion);
+    }
+
+    public List<PersonnageSuggestion> findAllUtilisateurs() {
+        return personnageSuggestionRepository.findAll();
     }
 
 }
